e702316089be3c9dae552823fe2d64f9cb3b73fd
Jelly Bardales <jellybardales@gmail.com>
2017-02-16 18:49:27 -0600

    layoutventana2

--- app/src/main/java/com/example/yely/maestrodetalle/MainActivity.java
+++ app/src/main/java/com/example/yely/maestrodetalle/MainActivity.java
@@ -9,6 +9,19 @@ public class MainActivity extends AppCompatActivity {
     //Definimos el ListView
     private ListView listView;
     //Elementos que se mostraran en el listview
-    private String lenguajeProgramacion[]=new String[]{"préstamos","equipos","más reciente","búsqueda","reciente","préstamos equipos",
+    private String lenguajeProgramacion[]=new String[]{"http://api.kivaws.org/v1/lenders/jeremy/loans.json","equipos","más reciente","búsqueda","reciente","préstamos equipos",
             "préstamos más reciente","préstamos búsqueda","métodos","socios"};
+    //Array de las imagenes
+
+
+
+    private String lenguajeProgramacion[]=new String[]{"http://api.kivaws.org/v1/lenders/jeremy/teams.json","http://api.kivaws.org/v1/lenders/newest.json","http://api.kivaws.org/v1/lenders/search.json","http://api.kivaws.org/v1/lending_actions/recent.json","http://api.kivaws.org/v1/loans/613086/teams.json","http://api.kivaws.org/v1/loans/newest.json",
+            "http://api.kivaws.org/v1/loans/search.json","http://api.kivaws.org/v1/methods.json","http://api.kivaws.org/v1/partners.json"};
+
+    private int[] LogoId = new int[]{R.drawable.java, R.drawable.php,
+            R.drawable.python,R.drawable.javascript,
+            R.drawable.ruby, R.drawable.c,R.drawable.go,
+            R.drawable.perl,R.drawable.pascal};
+
+
 }
